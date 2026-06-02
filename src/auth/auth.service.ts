33eedf79c03f478d49cd514e42f1a07ad3570896
vviolettaemerald <emeralddvioletta@gmail.com>
2026-06-02 08:33:07 +0700

    suspended account login restraint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,7 +19,24 @@ export class AuthService {
     if (user.status === 'suspended') {
       throw new UnauthorizedException('Akunmu di-suspend!')
     }
+    if (user.branchId) {
+      const branch = await this.prisma.branch.findUnique({
+        where: { id: user.branchId },
+      })
+      if (branch?.status === 'suspended') {
+        throw new UnauthorizedException('Cabang kamu di-suspend!')
+      }
+    }
 
+    // cek restaurant suspended
+    if (user.restaurantId) {
+      const restaurant = await this.prisma.restaurant.findUnique({
+        where: { id: user.restaurantId },
+      })
+      if (restaurant?.status === 'suspended') {
+        throw new UnauthorizedException('Restoran kamu di-suspend!')
+      }
+    }
     const isMatch = await bcrypt.compare(dto.password, user.password)
     if (!isMatch) {
       throw new UnauthorizedException('Email atau password salah!')
@@ -44,15 +61,19 @@ export class AuthService {
     }
   }
 
-  async me(userId: number) {
+  async me (userId: number) {
     return this.prisma.user.findUnique({
       where: { id: userId },
       select: {
-        id: true, name: true, email: true,
-        role: true, status: true,
-        restaurantId: true, branchId: true,
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        status: true,
+        restaurantId: true,
+        branchId: true,
         createdAt: true,
       },
-    });
+    })
   }
 }
